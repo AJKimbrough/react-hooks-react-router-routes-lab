ba346503b18c1437873c487c8396036094653e28
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-06-28 16:18:07 -0500

    complete lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+const director = directors.map(director => 
+  <div key={director.name}>
+    <h2>Name: {director.name}</h2>
+    Movies: 
+      {director.movies.map(movie => 
+        <li key={movie}>{movie}</li>)}
+    
+  </div>
+  )
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {director}
+  </div>
+  );
 }
 
 export default Directors;
